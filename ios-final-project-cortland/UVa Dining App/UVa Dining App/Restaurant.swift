21e75be162a47cfcf50776aa608f34fbbcb264f9
Unknown <mhc6kp@virginia.edu>
2017-11-28 22:49:44 -0500

    wake me up from the nightmare that is known as databases

--- ios-final-project-cortland/UVa Dining App/UVa Dining App/Restaurant.swift
+++ ios-final-project-cortland/UVa Dining App/UVa Dining App/Restaurant.swift
@@ -16,37 +16,65 @@ class Restaurant {
     var longitude: Double?
     var mondayOpeningHours: String?
     var mondayClosingHours: String?
+    var mondayOpenHours: String?
+    var mondayClosedHours: String?
     var mondayMealExchangeOpen: String?
     var mondayMealExchangeClosed: String?
+    var mondayMealOpeningHours: String?
+    var mondayMealClosingHours: String?
     
     var tuesdayOpeningHours: String?
     var tuesdayClosingHours: String?
+    var tuesdayOpenHours: String?
+    var tuesdayClosedHours: String?
     var tuesdayMealExchangeOpen: String?
     var tuesdayMealExchangeClosed: String?
+    var tuesdayMealOpeningHours: String?
+    var tuesdayMealClosingHours: String?
     
     var wednesdayOpeningHours: String?
     var wednesdayClosingHours: String?
+    var wednesdayOpenHours: String?
+    var wednesdayClosedHours: String?
     var wednesdayMealExchangeOpen: String?
     var wednesdayMealExchangeClosed: String?
+    var wednesdayMealOpeningHours: String?
+    var wednesdayMealClosingHours: String?
 
     var thursdayOpeningHours: String?
     var thursdayClosingHours: String?
+    var thursdayOpenHours: String?
+    var thursdayClosedHours: String?
     var thursdayMealExchangeOpen: String?
     var thursdayMealExchangeClosed: String?
+    var thursdayMealOpeningHours: String?
+    var thursdayMealClosingHours: String?
     
     var fridayOpeningHours: String?
     var fridayClosingHours: String?
+    var fridayOpenHours: String?
+    var fridayClosedHours: String?
     var fridayMealExchangeOpen: String?
     var fridayMealExchangeClosed: String?
+    var fridayMealOpeningHours: String?
+    var fridayMealClosingHours: String?
     
     var saturdayOpeningHours: String?
     var saturdayClosingHours: String?
+    var saturdayOpenHours: String?
+    var saturdayClosedHours: String?
     var saturdayMealExchangeOpen: String?
     var saturdayMealExchangeClosed: String?
+    var saturdayMealOpeningHours: String?
+    var saturdayMealClosingHours: String?
 
     var sundayOpeningHours: String?
     var sundayClosingHours: String?
+    var sundayOpenHours: String?
+    var sundayClosedHours: String?
     var sundayMealExchangeOpen: String?
     var sundayMealExchangeClosed: String?
+    var sundayMealOpeningHours: String?
+    var sundayMealClosingHours: String?
 
 }
